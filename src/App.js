05e0daca53f82bb2bd6bf97dcac67e24ff4a9097
Folafunmi <folafunmimustapha@gmail.com>
2020-09-21 15:08:30 +0100

    Added handleChange and handleSubmit function

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@
 
 import React from "react";
 import "./App.css";
+import TodoForm from "./components/todoForm";
 
 function App() {
   return (
     <div className="todo-app">
-      <h1>Todo App</h1>
+      <TodoForm />
     </div>
   );
 }
